8efc57076c1e665d629e73a2fb0a0050450d9c17
Kyle Rosenmeyer <kylerosenmeyer@gmail.com>
2018-09-30 23:46:53 -0400

    basic html/css

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+//RPS Multiplayer
+
+var config = {
+    apiKey: "AIzaSyBXY5RL7rbaS0achp2tpFw85yN7tcVk9uk",
+    authDomain: "rps-multiplayer-b7dae.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-b7dae.firebaseio.com",
+    projectId: "rps-multiplayer-b7dae",
+    storageBucket: "rps-multiplayer-b7dae.appspot.com",
+    messagingSenderId: "856828164304"
+  };
+  firebase.initializeApp(config);
